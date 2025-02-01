a43a3959e4bb57199ad4c0e50ccc4f9b32a7501a
Connor Bailey <cebailey92@gmail.com>
2025-02-01 13:13:27 -0500

    create lesson 3 branch

--- server.js
+++ server.js
@@ -1,14 +1,53 @@
 
 // Importing http module
-const http = require('http'); 
+const http = require('http');
+const fs = require('fs');
 
 // Creating a server
 const server = http.createServer((req, res) => {
-  console.log('Server is running...');
+  console.log(req.url, req.method);
+
+  // Setting the response header
+  res.setHeader('Content-Type', 'text/html');
+
+  // Find path of the requested URL
+  let path = './views/';
+  switch (req.url) {
+    case '/':
+      path += 'index.html';
+      res.statusCode = 200;
+      break;
+    case '/about':
+      path += 'about.html';
+      res.statusCode = 200;
+      break;
+    // Redirecting to /about
+    case '/about-me':
+      res.setHeader('Location', '/about');
+      res.statusCode = 301;
+      res.end();
+      break;
+    // Handling 404 page
+    default:
+      path += '404.html';
+      res.statusCode = 404;
+      break;
+  }
+
+  // Sending an HTML response
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end();
+    } else {
+      // res.write(data);
+      res.end(data);
+    }
+  });
+
 });
 
 // Listening to the server
 server.listen(3000, 'localhost', () => {
   console.log('Server is running on http://localhost:3000');
 });
-
