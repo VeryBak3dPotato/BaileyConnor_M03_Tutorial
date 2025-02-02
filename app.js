203083e6858c8b33707492c4e7d5cd324afd6333
Connor Bailey <cebailey92@gmail.com>
2025-02-02 15:34:28 -0500

    complete lesson 7

--- app.js
+++ app.js
@@ -1,14 +1,39 @@
 const express = require('express');
+const morgan = require('morgan');
 
-// Express app
+// Express App
 const app = express();
 
-// listen for requests
+// Listen For Requests
 app.listen(3000);
 
-// Register view engine
+// Register View Engine
 app.set('view engine', 'ejs');
 
+// Middleware & Static Files
+app.use(express.static('public'));
+
+app.use((req, res, next) => {
+    console.log('New Request Made:');
+    console.log('Host: ', req.hostname);
+    console.log('Path: ', req.path);
+    console.log('Method: ', req.method);
+    next();
+});
+
+app.use((req, res, next) => {
+    console.log('In the next middleware...');
+    next();
+});
+
+app.use(morgan('dev'));
+
+app.use((req, res, next) => {
+    res.locals.path = req.path;
+    next();
+});
+
+// Routes
 app.get('/', (req, res) => {
     const blogs = [
         { title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur' },
@@ -26,7 +51,7 @@ app.get('/blogs/create', (req, res) => {
     res.render('create', { title: 'Create a new blog' });
 });
 
-// 404 page
+// 404 Page
 app.use((req, res) => {
     res.status(404).render('404', { title: '404' });
 });
